8b6883187fe7cc662eba042b1e1fa847ff7348e5
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-11-06 12:16:04 +0900

    1106 12:15 PlayerController Customed

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rbody; //Rigidbody2D型の変数
     float axisH = 0.0f; //入力
+    public float speed = 3.0f; //移動速度
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,24 @@ public class PlayerController : MonoBehaviour
     {
         //水平方向の入力をチェックする
         axisH = Input.GetAxisRaw("Horizontal");
+
+        if(axisH > 0.0f)
+        {
+            //右移動
+            Debug.Log("右移動");
+            transform.localScale = new Vector2(1,1); //絵を右向き
+        }
+        else if (axisH < 0.0f)
+        {
+            //左移動
+            Debug.Log("左移動");
+            transform.localScale = new Vector2(-1, 1); //絵を左向き
+        }
     }
 
     void FixedUpdate()
     {
         //速度を更新する
-        rbody.velocity = new Vector2(axisH * 3.0f,rbody.velocity.y);
+        rbody.velocity = new Vector2(axisH * speed,rbody.velocity.y);
     }
 }
