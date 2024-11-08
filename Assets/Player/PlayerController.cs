f5824ece3d37cd44a5a46f420cd4b7d35a7a2051
Dyna-Rise <dynarise.headquarters@gmail.com>
2024-11-08 12:09:45 +0900

    1108 Jump Added

--- Assets/Player/PlayerController.cs
+++ Assets/Player/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     float axisH = 0.0f; //入力
     public float speed = 3.0f; //移動速度
 
+    public float jump = 9.0f; //ジャンプ力
+    public LayerMask groundLayer; //特定のレイヤー(Ground)情報を格納する変数 ※LayerMask型
+    bool goJump = false; //ジャンプ開始したかを見るフラグ
+
     // Start is called before the first frame update
     void Start()
     {
@@ -21,7 +25,7 @@ public class PlayerController : MonoBehaviour
         //水平方向の入力をチェックする
         axisH = Input.GetAxisRaw("Horizontal");
 
-        if(axisH > 0.0f)
+        if (axisH > 0.0f)
         {
             //右移動
             Debug.Log("右移動");
@@ -33,11 +37,49 @@ public class PlayerController : MonoBehaviour
             Debug.Log("左移動");
             transform.localScale = new Vector2(-1, 1); //絵を左向き
         }
+
+        //スペースキーがおされたかどうか
+        if (Input.GetButtonDown("Jump"))
+        {
+            Jump(); //ジャンプメソッドの発動
+        }
     }
 
     void FixedUpdate()
     {
-        //速度を更新する
-        rbody.velocity = new Vector2(axisH * speed,rbody.velocity.y);
+        //地上判定
+        //円のセンサーを設置して、指定した特定レイヤーに引っかかればtrue
+        bool onGround = Physics2D.CircleCast(
+            transform.position, //発射位置→プレイヤーの位置（足元）
+            0.2f, //円の半径
+            Vector2.down, //発射方向 <new Vector2(0,-1)>=Vector2.down
+            0.0f, //発射距離
+            groundLayer //対象レイヤー
+            );
+
+        //地面にいる時はvelocityがすべてに反応
+        //空中にいる時はvelocityは左右だけに反応
+        if (onGround || axisH != 0)
+        {
+            //速度を更新する
+            rbody.velocity = new Vector2(axisH * speed, rbody.velocity.y);
+        }
+
+        //地面にいる＆スペースキーが押された
+        if(onGround && goJump)
+        {
+            //後のAddForceメソッドの第一引数にいれる方向データをあらかじめ作っておく
+            Vector2 jumpPw = new Vector2(0,jump);
+            rbody.AddForce(jumpPw,ForceMode2D.Impulse); //jumpPwの方向に瞬間的な力を加えて押し出す
+            goJump = false; //ジャンプフラグを元に戻しておく
+        }
     }
+
+    //自作メソッド ジャンプフラグを立てるメソッド
+    public void Jump()
+    {
+        goJump = true; //ジャンプ開始されたフラグがtrue
+    }
+
 }
+
